37f6e53a14026a0bbef8355a75c770aa2a9209b1
Max <mczonk@gmail.com>
2014-12-04 02:46:55 +0100

    Add comment

--- Hitbox/HitboxStreamIngest.h
+++ Hitbox/HitboxStreamIngest.h
@@ -8,6 +8,11 @@
 @property (nonatomic, copy, readonly) NSURL *URL;
 @property (nonatomic, copy, readonly) NSString *name;
 
+/**
+ * Builds a full URL
+ * @param streamPath use HitboxStreamIngestResponse.streamPath as parameter
+ * @return URL to stream to including the stream key
+ */
 - (NSURL *)URLWithStreamPath:(NSString *)streamPath;
 
 @end
